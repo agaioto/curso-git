a18ff3612e1171398136f41ce805435f25bbd97a
agaioto <angelo.r.gaioto@gmail.com>
2018-11-19 10:15:34 -0200

    Alteração no cadastro de produtos

--- curso/Program.cs
+++ curso/Program.cs
@@ -15,8 +15,8 @@ namespace curso {
             produtos.Add(new Produto(1001, "Cadeira simples", 500.00));
             produtos.Add(new Produto(1002, "Cadeira acolchoada", 900.00));
             produtos.Add(new Produto(1003, "Sofá de três lugares", 2000.00));
-            produtos.Add(new Produto(1004, "Mesa retangular", 1500.00));
             produtos.Add(new Produto(1005, "Mesa retangular", 2000.00));
+            produtos.Add(new Produto(1005, "Novo produto adicionado", 2000.00));
             produtos.Sort();
 
             while (opcao != 5) {
